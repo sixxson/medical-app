38b0dbecc2d59692ac93f9300139eaaa9927ebd7
minh le <117999133+sixxson@users.noreply.github.com>
2024-09-30 12:31:00 +0700

    feat- ceart component dashboard

---
+++ app/(back)/dashboard/doctor/inbox/use-email.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UseEmail() {
+  return (
+    <div>use-email</div>
+  )
+}
--- app/(back)/dashboard/page.tsx
+++ app/(back)/dashboard/page.tsx
@@ -1,8 +1,33 @@
 import Dashboard from "@/components/Dashboard/Dashboard"
+import DoctorDashBoard from "@/components/Dashboard/DoctorDashBoard";
+import PatientsDashBoard from "@/components/Dashboard/PatientsDashBoard";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user
+  const role = user?.role
+
+   if(role === "USER"){
+        return (
+            <div>
+                <p>The User Role is {user?.role}</p>
+                <PatientsDashBoard />
+            </div>
+        )
+    }
+    if(role === "DOCTOR"){  
+        return (
+            <div>
+                <p>The User Role is {user?.role}</p>
+                <DoctorDashBoard />
+            </div>
+        )
+    }
   return (
     <div>
+      <p>The User Role is {user?.role}</p>
       <Dashboard />
     </div>
   )
